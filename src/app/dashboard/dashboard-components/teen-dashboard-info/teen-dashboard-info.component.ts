87877b5d631fa034dc62a7f2dedeb2eef3b8ffa0
Carlos Sanchez <carlos.sanchez@vallegrande.edu.pe>
2023-11-06 20:59:27 -0500

    Update Code: Update (Paginator) Table in General

--- src/app/dashboard/dashboard-components/teen-dashboard-info/teen-dashboard-info.component.ts
+++ src/app/dashboard/dashboard-components/teen-dashboard-info/teen-dashboard-info.component.ts
@@ -1,74 +1,75 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {TeenService} from 'src/app/components/component-funcionality/services/teen/teen.service';
 import {MatPaginator} from "@angular/material/paginator";
-import { MatTableDataSource } from '@angular/material/table';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
-    selector: 'app-teen-dashboard-info',
-    templateUrl: './teen-dashboard-info.component.html',
-    styleUrls: ['./teen-dashboard-info.component.scss'],
+  selector: 'app-teen-dashboard-info',
+  templateUrl: './teen-dashboard-info.component.html',
+  styleUrls: ['./teen-dashboard-info.component.scss'],
 })
-export class TeenDashboardInfoComponent implements OnInit, AfterViewInit {
+export class TeenDashboardInfoComponent implements OnInit {
 
-    @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-    teenDatas!: MatTableDataSource<any>;
-    teenData: any[] = [];
-    operativeUnitData: any[] = [];
-    displayedColumns: string[] = [
-        'id',
-        'name',
-        'surnames',
-        'phoneNumber',
-        'operativeUnit',
-    ];
+  teenData: any[] = [];
+  operativeUnitData: any[] = [];
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'surnames',
+    'phoneNumber',
+    'operativeUnit',
+  ];
 
-    constructor(public _teenService: TeenService) {
+  // Manejar el paginator de la tabla (Teen)
+  dataSource = new MatTableDataSource(this.teenData);
+
+  constructor(public _teenService: TeenService) {
+  }
+
+  ngOnInit(): void {
+    this.findAllDataCompleteTeen();
+    this.findAllDataActiveTeen();
+    this.findAllDataCompleteOperativeUnit();
+  }
+
+  findAllDataCompleteTeen() {
+    this._teenService.findAllDataActive().subscribe((DataTeenBD: any) => {
+      //console.log('Data Teen:', DataTeenBD);
+      // this.teenData = DataTeenBD;
+    });
+  }
+
+  findAllDataActiveTeen() {
+    this._teenService
+      .findAllDataActive()
+      .subscribe((DataTeenBDActive: any) => {
+        //console.log('Data Teen Active:', DataTeenBDActive);
+        this.teenData = DataTeenBDActive;
+        this.dataSource = new MatTableDataSource(this.teenData);
+        this.dataSource.paginator = this.paginator;
+      });
+
+  }
+
+  findAllDataCompleteOperativeUnit() {
+    this._teenService
+      .findAllDataOperativeUnit()
+      .subscribe((dataOperativeUnit: any) => {
+        this.operativeUnitData = dataOperativeUnit;
+      });
+  }
+
+  getDataInformationOperativeUnit(id_operativeunit: number) {
+    const soa = this.operativeUnitData.find(
+      (item) => item.id_operativeunit === id_operativeunit
+    );
+    if (soa) {
+      return `${soa.name}`;
+    } else {
+      return 'Unidad Operativa asignada no fue encontrada.';
     }
+  }
 
-    ngOnInit(): void {
-        this.findAllDataCompleteTeen();
-        this.findAllDataActiveTeen();
-        this.findAllDataCompleteOperativeUnit();
-    }
-
-    findAllDataCompleteTeen() {
-        this._teenService.findAllDataActive().subscribe((DataTeenBD: any) => {
-            //console.log('Data Teen:', DataTeenBD);
-            // this.teenData = DataTeenBD;
-        });
-    }
-
-    findAllDataActiveTeen() {
-        this._teenService
-            .findAllDataActive()
-            .subscribe((DataTeenBDActive: any) => {
-                //console.log('Data Teen Active:', DataTeenBDActive);
-                this.teenData = DataTeenBDActive;
-            });
-
-    }
-
-    findAllDataCompleteOperativeUnit() {
-        this._teenService
-            .findAllDataOperativeUnit()
-            .subscribe((dataOperativeUnit: any) => {
-                this.operativeUnitData = dataOperativeUnit;
-            });
-    }
-
-    getDataInformationOperativeUnit(id_operativeunit: number) {
-        const soa = this.operativeUnitData.find(
-            (item) => item.id_operativeunit === id_operativeunit
-        );
-        if (soa) {
-            return `${soa.name}`;
-        } else {
-            return 'Unidad Operativa asignada no fue encontrada.';
-        }
-    }
-
-    ngAfterViewInit(): void {
-        
-    }
 }
